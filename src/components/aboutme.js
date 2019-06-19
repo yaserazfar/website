3aee0af75890226c2220022902cb351ed95a4a6e
Yaser Azfar <yaserazfar@gmail.com>
2019-06-20 11:53:15 +1200

    Add styling and basic improvements to Ideas page, and add more info to About Me page

--- src/components/aboutme.js
+++ src/components/aboutme.js
@@ -15,12 +15,12 @@ class About extends Component {
                             <p>
                             After gaining experience of working in an agile start-up company, I am really intrigued by opportunities of having a positive input in software projects. I am currently in my second year of university studying software engineering, where I code mostly in Java and C#, while also learning SQL, Python and have gained experience in project management and worked in multiple group projects.<br></br><br></br>
 
-In my previous software engineering job I worked in a Platform as a Service team, where I was responsible for transferring a mono-repository into a multi-repository for the micro-service architecture of the platform. Following that I was transferred to the Web Development team, where I worked on various bugs for the team, while also creating tests for the CI pipeline of the website.<br></br><br></br>
+                            In my previous software engineering job I worked in a Platform as a Service team, where I was responsible for transferring a mono-repository into a multi-repository for the micro-service architecture of the platform. Following that I was transferred to the Web Development team, where I worked on various bugs for the team, while also creating tests for the CI pipeline of the website.<br></br><br></br>
 
-Overall, I am looking to explore and go deeper into the many fields of software development to increase my knowledge to be able to have an impact in the world.
+                            Overall, I am looking to explore and go deeper into the many fields of software development to increase my knowledge to be able to have an impact in the world.
                             </p>
 
-                            <p> Java | C# | Python | C++ | React | JavaScript | SQL </p>
+                            <p> Java | C# | Python | C++ | React | JavaScript | SQL | Git | Jenkins </p>
                         </div>
                     </Cell>
                 </Grid>
--- src/components/ideas.js
+++ src/components/ideas.js
@@ -2,40 +2,66 @@ import React, { Component } from 'react';
 import { Form, FormControl, FormGroup, FormLabel, Button} from 'react-bootstrap';
 
 class Contact extends Component {
+    constructor(){
+        super();
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    handleSubmit(event) {
+        event.preventDefault();
+        const data = new FormData(event.target);
+
+        //const name = data.get(name);
+        // const email = data.get(email);
+        // const text = data.get(text);
+
+        // data.set(name, email, text);
+        
+        fetch('/api/form-submit-url', {
+          method: 'POST',
+          body: data,
+        });
+      }
+
     render() {
         return(
-            <Form>
 
-             <h2>Give me reccomendations on what to learn about, or leave feedback for this website! </h2> 
-                    <FormGroup controlId="formBasicName">
-                        <FormLabel>Name: </FormLabel>
-                        <FormControl type="name" placeholder="Enter full name" />
-                    </FormGroup>
+            <div className="form-background">
 
-                    <FormGroup controlId="formBasicEmail">
-                        <FormLabel>Email address: </FormLabel>
-                        <FormControl type="email" placeholder="Enter email" />
-                    </FormGroup>
+                <Form className="form-page">
 
-                    <FormGroup controlId="formControlSelect">
-                        <FormLabel> Category: </FormLabel>
-                        <FormControl as="select">
-                            <option>Software</option>
-                            <option>Book</option>
-                            <option>Person</option>
-                            <option>Other</option>
-                            <option>Feedback</option>
-                        </FormControl>
-                    </FormGroup>
+                <h2>Give me reccomendations on what to learn or give feedback for this website! </h2> 
 
-                    <FormGroup controlId="formControlTextArea">
-                        <FormLabel>Idea / Recommendation: </FormLabel>
-                        <FormControl as="textarea" rows="3"/>
-                    </FormGroup>
+                        <FormGroup controlId="formBasicName">
+                            <FormLabel>Name: </FormLabel>
+                            <FormControl type="name" placeholder="Enter full name" />
+                        </FormGroup>
 
-                    <Button variant="primary" type="submit"> Submit </Button>
+                        <FormGroup controlId="formBasicEmail">
+                            <FormLabel>Email address: </FormLabel>
+                            <FormControl type="email" required placeholder="Enter email" />
+                        </FormGroup>
 
-                </Form>
+                        <FormGroup controlId="formControlSelect">
+                            <FormLabel> Category: </FormLabel>
+                            <FormControl as="select">
+                                <option>Software</option>
+                                <option>Book</option>
+                                <option>Person</option>
+                                <option>Other</option>
+                                <option>Feedback</option>
+                            </FormControl>
+                        </FormGroup>
+
+                        <FormGroup controlId="formControlTextArea">
+                            <FormLabel>Recommendation / Feedback: </FormLabel>
+                            <FormControl type="text" as="textarea" required rows="3"/>
+                        </FormGroup>
+
+                        <Button variant="primary" type="submit"> Submit </Button>
+
+                    </Form>
+            </div>
 
         )
     }
